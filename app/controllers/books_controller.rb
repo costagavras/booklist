734108e58466310f2cff6aaf333e1af97d77f00c
costagavras <cgavras@yahoo.ca>
2018-10-23 15:13:27 -0400

    response types

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,22 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv do #generates and downloads CSV file
+        render plain: Book.generate_csv(@books)
+      end
+      format.json do
+        render json: @books.map { |book|
+          { name: book.title,
+            author: book.author,
+            read_status: book.already_read
+          }
+        }
+      end
+    end
   end
 
 end
